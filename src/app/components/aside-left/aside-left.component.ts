d7d05608f2d2b17fa80c2ad16f6020d9f04fb078
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2017-07-05 09:52:09 +0300

    show list of section

--- src/app/components/aside-left/aside-left.component.ts
+++ src/app/components/aside-left/aside-left.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+
 const SECTIONS: string[] = ["Metadata", "Thumbnails", "AccessControl", "Scheduling", "Flavors", "Captions", "Live", "Related", "Clips", "Users"];
+
 @Component({
   selector: 'aside-left',
   templateUrl: './aside-left.component.html',
   styleUrls: ['./aside-left.component.scss']
 })
 export class AsideLeftComponent {
+
+  sections = SECTIONS;
+
+  selectedSection: string;
+
+  onSelect(section: string): void {
+    this.selectedSection = section;
+  }
 }
