aef04f7cff9ffc6f54307f346dd7ba026548e175
Amishai Guilburd <amishai.guilburd@kaltura.com>
2017-07-06 10:20:43 +0300

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './components/parent/app.component';
-import { MetaComponent } from './components/meta/meta.component';
+import { MetaFormComponent } from './components/meta/meta.component';
 import { PreviewComponent } from './components/preview/preview.component';
 import { SectionComponent } from './components/section/section.component';
 import { EntryDetailsService } from './components/section/section.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MetaComponent,
+    MetaFormComponent,
     PreviewComponent,
     SectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [EntryDetailsService],
   bootstrap: [AppComponent]
--- src/app/components/meta/meta.component.ts
+++ src/app/components/meta/meta.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-meta-selector',
   templateUrl: './meta.component.html',
   styleUrls: ['./meta.component.scss']
 })
-export class MetaComponent {
+export class MetaFormComponent {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      id: new FormControl({ value: '1234', disabled: true }),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      description: new FormControl('description'),
+      referenceId: new FormControl('reference id')
+    });
+  }
+
+  onSubmit(metaData) {
+    console.log(metaData);
+  }
+
 }
